94094c10861e1f9ef08657274b2de5d219342afb
ahidalgoenrico <ahidalgoenrico@move-me-service.com>
2023-07-09 14:55:09 -0300

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+  if(Number.isSafeInteger(startNumber) && 
+    Number.isSafeInteger(endNumber) &&
+    startNumber >= 0 &&
+    endNumber >= 0
+  ){
+    if(endNumber < startNumber) {
+      const aux = endNumber;
+      endNumber = startNumber;
+      startNumber = aux;
+    }
 
+    let sum = 0;
+    for(let i = startNumber; i <= endNumber ; i++){
+      sum += i;
+    }
+    return sum;
+  } else {
+    return 'ERROR'
+  }
 };
 
 // Do not edit below this line
