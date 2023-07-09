bcc357fe2f5539ad131f9f347d7018ab284838e2
ahidalgoenrico <ahidalgoenrico@move-me-service.com>
2023-07-08 23:17:11 -0300

    Solve exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+  let result = '';
+  if(numberOfRepeats < 0){
+    return 'ERROR';
+  } else {
+    for(let i = 0; i < numberOfRepeats; i++){
+      result += string;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
