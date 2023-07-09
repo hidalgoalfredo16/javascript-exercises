613ac398b9d2cd6e88c060c620dbc630d5962510
ahidalgoenrico <ahidalgoenrico@move-me-service.com>
2023-07-09 15:18:27 -0300

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitValue) {
+  return parseFloat(((fahrenheitValue - 32) * 5 / 9).toFixed(1)); 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusValue) {
+  return parseFloat((celsiusValue * 9 / 5).toFixed(1)) + 32;
 };
 
 // Do not edit below this line
